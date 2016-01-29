c7d4e23b991013cc2e11af5ae103a7aed2e37356
upamune <jajkeqos@gmail.com>
2016-01-29 20:38:54 +0900

    :rocket: implement GET method

---
+++ api/polls.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type poll struct {
+	ID      bson.ObjectId  `bson:"_id" json:"id"`
+	Title   string         `json:"title"`
+	Options []string       `json:"options"`
+	Results map[string]int `json:"results,omitempty"`
+}
+
+func handlePolls(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handlePollsGet(w, r)
+		return
+	case "POST":
+		handlePollsPost(w, r)
+		return
+	case "DELETE":
+		handlePollsDelete(w, r)
+		return
+	}
+	respondHTTPErr(w, r, http.StatusNotFound)
+}
+
+func handlePollsGet(w http.ResponseWriter, r *http.Request) {
+	db := GetVar(r, "db").(*mgo.Database)
+	c := db.C("polls")
+	var q *mgo.Query
+	p := NewPath(r.URL.Path)
+	if p.HasID() {
+		q = c.FindId(bson.ObjectIdHex(p.ID))
+	} else {
+		q = c.Find(nil)
+	}
+	var result []*poll
+
+	if err := q.All(&result); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+	respond(w, r, http.StatusOK, &result)
+}
+
+func handlePollsPost(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
+
+func handlePollsDelete(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
